654163871824a500ac420507f0bfea08cbeacb71
Amanda D <amanda@depaula.xyz>
2020-09-25 21:47:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,8 +13,8 @@ end
 
 def get_japanese_emoticon(emo_path,emoticon)
   load_library(emo_path).each do |key, value|
-   if value == emoticon
-      return value
+   if value[:japanese] == emoticon
+
          binding.pry
     end
   end
